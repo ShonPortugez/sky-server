fbe08e98c91bb76c3844a4a46efe7a7fbe6364ab
Shon Portugez <shonportugez@gmail.com>
2025-12-17 19:12:39 +0200

    refactor: Removed obsolete files

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from 'express';
-import authRoutes from "./authRoutes";
-import userRoutes from "./userRoutes";
-
-export const registerRoutes = (app: Express) => {
-    app.use('/api/auth', authRoutes);
-    app.use('/api/users', userRoutes)
-};
